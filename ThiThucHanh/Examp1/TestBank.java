f88b8313699acfe8d22c75acf9401dfba8051451
EdanPrince <112059492+EdanPrince@users.noreply.github.com>
2023-03-03 09:16:48 +0700

    Delete ThiThucHanh directory

--- ThiThucHanh/Examp1/TestBank.java
+++
@@ -1,7 +0,0 @@
-public class TestBank {
-    public static void main(String[] args) {
-        Bank bank = new Bank(1000.0, 10.0);
-        double interest = bank.calculateInterest();
-        System.out.println("Monthly interest: $" + interest);
-    }
-}
